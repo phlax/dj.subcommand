3d0389ca35f08349e4e78c44bd13ba5a346b9a35
Ryan Northey <ryan@synca.io>
2017-11-25 23:13:43 +0000

    Add test settings

---
+++ tests/settings.py
@@ -0,0 +1,13 @@
+
+SECRET_KEY = "NOTSOSECRET"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'ATOMIC_REQUESTS': True,
+    }
+}
